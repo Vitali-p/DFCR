1318d6f2e1ada1049f2b70e4145ff788d6448e7b
TokeA <toke_tt@hotmail.com>
2017-01-05 13:24:22 +0100

    Circular with zerocrossing counting

--- Software uC/app/main.c
+++ Software uC/app/main.c
@@ -3,15 +3,19 @@
 #include "board.h"
 #include "sys.h"
 #include "ADC.h"
+#include <stdbool.h>
 //#include "sdram_64M_32bit_drv.h"
 
 #include "smb380_drv.h"
 #include "Timer.h"
 
 Int32U _ADCVal;
-Int32U  _ADCStatus;
+Int32U _ADCStatus;
 Int32U _regCount = 0;
-Int32U  _waveForm[1000];
+Int32U _waveForm[1000];
+Boolean CountFlag = false;
+Int32U _NumberOfCrossings = 0;
+Int32U _Debugvar = 0;
 
 #define NONPROT 0xFFFFFFFF
 #define CRP1  	0x12345678
@@ -32,11 +36,14 @@ void Timer0IntrHandler (void)
   
   T0IR_bit.MR0INT = 1; // Clear the timer 0 interrupt.
   VICADDRESS = 0;
-  _ADCVal = ((AD0GDR & 0xFFC0)>>6);
-  _waveForm[_regCount] = _ADCVal;
-  _regCount++;
-  if(_regCount == 1000){
+  
+  // ADC Related
+  _ADCVal = ((AD0GDR & 0xFFC0)>>6);  //Get ADC Value
+  _waveForm[_regCount] = _ADCVal;    // Store ADC Value in circular register
+  _regCount++;                       //Increment regcount
+  if(_regCount == 1000){             //Check if regcount need to start over
   _regCount = 0;
+  CountFlag = true;
   }
 }
 
@@ -65,7 +72,7 @@ int main(void)
   USB_D_LINK_LED_FSET = USB_D_LINK_LED_MASK;
 
   //Timer init ---
-  Timer0Init(1);
+  Timer0Init(10000);
   
   //Init timer 0 interrupt:
   VIC_SetVectoredIRQ(Timer0IntrHandler,0,VIC_TIMER0);
@@ -73,7 +80,16 @@ int main(void)
   T0TCR_bit.CE = 1;     // Enable counting.
   initADC2();
   while(1){
-  
-  
+    if(CountFlag == true){
+      _NumberOfCrossings = 0;
+      for (Int32U ii = 1; ii <= 999; ii++){
+        
+        if((_waveForm[ii]<511)^(_waveForm[ii + 1]<511)){
+        _NumberOfCrossings++;
+        }
+      }
+      CountFlag = false;
+      _Debugvar++;
+    }
   }
 }
