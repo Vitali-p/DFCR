363c64948031685684cb80f74634621a8d146a9a
TokeA <toke_tt@hotmail.com>
2017-01-16 18:22:33 +0100

    Trying to merge with uIP

--- Software uC/app/includes.h
+++ Software uC/app/includes.h
@@ -37,6 +37,39 @@
 #include "autoOFF.h"
 #include "buttomON.h"
 #include "buttomOFF.h"
+   
+   
+   ///////////////////////////////////////////////////////////////////////
+   /////////////////////   INCLUDES FOR UIP        ///////////////////////
+   ///////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <intrinsics.h>
+#include <assert.h>
+#include <nxp/iolpc2478.h>
 
+#include "arm_comm.h"
+#include "board.h"
+#include "sys.h"
+   
+#include "sdram_64M_32bit_drv.h"
+#include "drv_glcd.h"
 
+#include "clock-arch.h"
+
+#include "timer.h"
+#include "uip-conf.h"
+#include "uipopt.h"
+#include "uip_arp.h"
+#include "uip.h"
+#include "tapdev.h"
+#include "httpd.h"
+   ///////////////////////////////////////////////////////////////////////
+   ///////////////////   END INCLUDES FOR UIP        /////////////////////
+   ///////////////////////////////////////////////////////////////////////
+   
 #endif  // __INCLUDES_H
